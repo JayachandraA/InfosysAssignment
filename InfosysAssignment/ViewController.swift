a3cc005800294701dd4df3eaf7354fe6a769e8ee
Jayachandra <jayachandra.a@bluerose-tech.com>
2019-12-13 17:56:12 +0530

    Initial Commit

---
+++ InfosysAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InfosysAssignment
+//
+//  Created by Jayachandra on 12/13/19.
+//  Copyright © 2019 BlueRose Technologies Pvt Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
